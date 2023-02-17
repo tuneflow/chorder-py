de9d7793d01da1256f5616e800100e5b93db27d4
Andantei <zaoyinji@gmail.com>
2023-02-16 20:11:07 -0800

    update package info

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="chorder",
+    name="chorder_py",
     version="1.0.0",
-    author="Mingzhi Zeng",
-    author_email="zeostudio@gmail.com",
+    author="TuneFlow",
+    author_email="contact@info.tuneflow.com",
     description="A modified version of chorder with drums fix",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitee.com/panacea123/chorder",
+    url="https://github.com/tuneflow/chorder-py",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
